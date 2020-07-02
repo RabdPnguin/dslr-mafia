09e96ffffde29b011e5c2ed8aff178b386a0ab3a
Jeremy Capello <me@lethargicdeveloper.com>
2020-07-02 00:00:03 -0500

    added server

---
+++ server/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace DslrMafia
+{
+  public class Program
+  {
+    public static void Main(string[] args) =>
+      CreateWebHostBuilder(args).Build().Run();
+
+    public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+      WebHost.CreateDefaultBuilder(args)
+        .UseStartup<Startup>()
+        .UseKestrel((context, options) =>
+        {
+          var port = Environment.GetEnvironmentVariable("PORT");
+          if (!string.IsNullOrEmpty(port))
+          {
+            options.ListenAnyIP(int.Parse(port));
+          }
+        });
+  }
+}
---
+++ server/Startup.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DslrMafia
+{
+  public class Startup
+  {
+    private readonly IConfiguration config;
+
+    public Startup(IConfiguration config)
+    {
+      this.config = config;
+    }
+
+    [System.Obsolete]
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+      services.AddNodeServices();
+      services.AddCors();
+      services.AddMemoryCache();
+
+      services.AddSpaStaticFiles(spa => spa.RootPath = "../client/build");
+    }
+
+    [System.Obsolete]
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    {
+      app.UseCors(config => config
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+        .WithOrigins("http://localhost:3000")
+        .AllowCredentials());
+
+      app.UseStaticFiles();
+      app.UseSpaStaticFiles();
+      app.UseMvc();
+      app.UseSpa(spa => spa.Options.SourcePath = "../client");
+    }
+  }
+}
