ce0def73667143e0f06ec58d3b2542cded1eb509
Jeremy Capello <me@lethargicdeveloper.com>
2020-07-02 22:54:38 -0500

    updated defaults

--- client/src/atoms.js
+++ client/src/atoms.js
@@ -1,21 +1,8 @@
 import { atom } from 'recoil';
-import { v4 as uuid } from 'uuid';
 
 export const playersState = atom({
   key: 'players',
-  default: [{
-    key: uuid(),
-    name: 'rabidpenguin',
-    aliases: 'rp, rabid'
-  }, {
-    key: uuid(),
-    name: 'El Quintron',
-    aliases: 'elq'
-  }, {
-    key: uuid(),
-    name: 'Chaplain',
-    aliases: 'chap'
-  }],
+  default: [],
   persistence_UNSTABLE: {
     type: 'players'
   }
